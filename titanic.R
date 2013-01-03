076cbcc602e1e739cfa2ce35a3fa2d493024c223
redmode <gedranovich@gmail.com>
2013-01-03 05:41:48 +0300

    Description stats

--- titanic.R
+++ titanic.R
@@ -1,1 +1,21 @@
 setwd("/home/ales/docs/Dropbox/Docs/dev/titanic-kaggle")
+
+# Read data
+titanic.train <- read.csv("train.csv")
+titanic.test  <- read.csv("test.csv")
+attach(titanic.train)
+
+##
+## Summarizing
+##
+library(plyr)
+
+## sex and pclass are valuable
+ddply(titanic.train, .(pclass,sex), summarize,
+      total=length(pclass),
+      survived=sum(survived),
+      percentage=survived/total)
+
+## fare can be used as a feature within pclass
+aggregate(fare~pclass, titanic.train, mean)
+sapply(sort(unique(pclass)), function(x) summary(fare[pclass==x]))
